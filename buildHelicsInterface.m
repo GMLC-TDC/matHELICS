26d4290dc1cbbac70b6485864209032f56311e89
Philip Top <top1@llnl.gov>
2022-07-20 06:04:58 -0700

    update the build script

--- buildHelicsInterface.m
+++ buildHelicsInterface.m
@@ -1,60 +1,72 @@
 
+function buildHelicsInterface(targetPath)
+if (nargin==0)
+    targetPath=fileparts(mfilename('fullpath'));
+end
+if (~exist(targetPath,'dir'))
+    makedir(targetPath);
+end
+inputPath=fileparts(mfilename('fullpath'));
 HelicsVersion='3.2.1';
 % set up platform specific names and locations
+targetTarFile=fullfile(targetPath,['helicsTar',HelicsVersion,'.tar.gz']);
 if ismac
-    basePath=['Helics-',HelicsVersion,'-macOS-universal2'];
+    basePath=fullfile(targetPath,['Helics-',HelicsVersion,'-macOS-universal2']);
     baseFile=['Helics-shared-',HelicsVersion,'-macOS-universal2.tar.gz'];
     % download the helics library if needed
     if (~exist(fullfile(basePath,'include/helics/helics.h'),'file'))
-        if (~exist('helicsTar.tar.gz','file'))
-            websave('helicsTar.tar.gz',['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
+        if (~exist(targetTarFile,'file'))
+            websave(targetTarFile,['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
         end
-        !tar xf helicsTar.tar.gz
+        system(['tar xf ',targetTarFile,' -C ',targetPath]);
     end
      %actually build the mex file
-    mex('-lhelics','-R2018a',['-I./',basePath,'/include/'],['-L./',basePath,'/lib'],'helicsMex.cpp')
+    mex('-lhelics','-R2018a',['-I',basePath,'/include/'],['-L',basePath,'/lib'],['-Wl,-rpath,',basePath,'/lib'],fullfile(inputPath,'helicsMex.cpp'),'-outdir',targetPath)
 elseif isunix
-    basePath=['Helics-',HelicsVersion,'-Linux-x86_64'];
+    basePath=fullfile(targetPath,['Helics-',HelicsVersion,'-Linux-x86_64']);
     baseFile=['Helics-shared-',HelicsVersion,'-Linux-x86_64.tar.gz'];
     % download the helics library if needed
     if (~exist(fullfile(basePath,'include/helics/helics.h'),'file'))
-        if (~exist('helicsTar.tar.gz','file'))
-            websave('helicsTar.tar.gz',['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
+        if (~exist(targetTarFile,'file'))
+            websave(targetTarFile,['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
         end
-        !tar xf helicsTar.tar.gz
+        system(['tar xf ',targetTarFile,' -C ',targetPath]);
     end
      %actually build the mex file
-    mex('-lhelics','-R2018a',['-I./',basePath,'/include/'],['-L./',basePath,'/lib'],'helicsMex.cpp')
+    mex('-lhelics','-R2018a',['-I',basePath,'/include/'],['-L',basePath,'/lib'],['-Wl,-rpath,',basePath,'/lib'],fullfile(inputPath,'helicsMex.cpp'),'-outdir',targetPath)
 elseif ispc
     if isequal(computer,'PCWIN64')
-        basePath=['Helics-',HelicsVersion,'-win64'];
+        basePath=fullfile(targetPath,['Helics-',HelicsVersion,'-win64']);
         baseFile=['Helics-shared-',HelicsVersion,'-win64.tar.gz'];
         targetFile='helics.dll';
     else
-        basePath=['Helics-',HelicsVersion,'-win32'];
+        basePath=fullfile(targetPath,['Helics-',HelicsVersion,'-win32']);
         baseFile=['Helics-shared-',HelicsVersion,'-win32.tar.gz'];
         targetFile='helics.dll';
     end
     % download the helics library if needed
     if (~exist(fullfile(basePath,'include/helics/helics.h'),'file'))
-        if (~exist('helicsTar.tar.gz','file'))
-            websave('helicsTar.tar.gz',['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
+        if (~exist(targetTarFile,'file'))
+            websave(targetTarFile,['https://github.com/GMLC-TDC/HELICS/releases/download/v',HelicsVersion,'/',baseFile]);
         end
-        untar('helicsTar.tar.gz');
+        untar(targetTarFile,targetPath);
 
     end
     %actually build the mex file
-    mex('-lhelics','-R2018a',['-I./',basePath,'/include/'],['-L./',basePath,'/lib'],['-L./',basePath,'/bin'],'helicsMex.cpp')
+    mex('-lhelics','-R2018a',['-I',basePath,'/include/'],['-L',basePath,'/lib'],['-L',basePath,'/bin'],fullfile(inputPath,'helicsMex.cpp'),'-outdir',targetPath)
     %copy the needed dll file if on windows
     if ispc
-        if (~exist(targetFile,'file'))
-            copyfile(fullfile(basePath,'bin',targetFile),'.');
+        if (~exist(fullfile(targetPath,targetFile),'file'))
+            copyfile(fullfile(basePath,'bin',targetFile),fullfile(targetPath,targetFile));
         end
     end
 else
     error('Platform not supported');
 end
 
+%% now build the interface directory
+copyfile(fullfile(inputPath,'matlabBindings','+helics'),fullfile(targetPath,'+helics'));
+copyfile(fullfile(inputPath,'extra_m_codes'),fullfile(targetPath,'+helics'));
 
 
 
